31ea55322b9656083794756beebc2537afed407a
noam.miron <noam.e.miron@gmail.com>
2023-12-07 14:14:17 +0200

    remove hardcoded secret templates

--- db/main.tf
+++ db/main.tf
@@ -5,8 +5,6 @@ terraform {
     container_name       = "tfstate"
     key                  = "db.tfstate"
     use_oidc             = true
-    subscription_id      = "00000000-0000-0000-0000-000000000000"
-    tenant_id            = "00000000-0000-0000-0000-000000000000"
   }
 }
 data "azurerm_client_config" "current" {}
