d297fa7f1d91a284c2268a8fa6b1b2d1527c0e7a
noam.miron <noam.e.miron@gmail.com>
2023-12-07 22:16:51 +0200

    added query logging

--- function/function.py
+++ function/function.py
@@ -1,54 +1,49 @@
 import azure.functions as func
-from flask import Flask, request, jsonify
-from azure.cosmos import CosmosClient
+import json
 import datetime
+from azure.cosmos import CosmosClient
 
-app = Flask(__name__)
-
-# Replace these values with your Cosmos DB connection details
-endpoint = 'your_cosmos_db_endpoint'
-key = 'your_cosmos_db_key'
-database_name = 'your_database_name'
-log_collection_name = 'log_collection'
-query_collection_name = 'query_collection'
-
-# Initialize Cosmos DB client
-client = CosmosClient(endpoint, key)
-database = client.get_database_client(database_name)
-log_container = database.get_container_client(log_collection_name)
-query_container = database.get_container_client(query_collection_name)
-
-@app.route('/search', methods=['POST'])
-def search(req: func.HttpRequest):
-    try:
-        data = request.get_json()
-
-        # Log the query and query time to Cosmos DB
-        log_entry = {
-            'query': data,
-            'query_time': str(datetime.datetime.utcnow())
-        }
-        log_container.upsert_item(log_entry)
-
-        # Perform the query on another collection
-        # Replace this query logic with your actual query
-        query_result = query_container.query_items(
-            query='SELECT * FROM c WHERE c.name = @name',
-            parameters=[
-                dict(name='@name', value=data['name'])
-            ]
-        )
-
-        # Extract and format the result
-        result = [item for item in query_result]
-
-        return jsonify(result)
-
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-func_app = func.FlaskFunction(app)
-
-# Azure Functions HTTP trigger
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func_app(req)
+
+    req_body = req.get_json()
+    name = req_body.get('name', '')
+    style = req_body.get('style', '')
+    address = req_body.get('address', '')
+    is_veg = req_body.get('isVeg', False)
+    is_open = req_body.get('isOpen', False)
+
+    cosmos_db_connection_string = ""
+
+    client = CosmosClient.from_connection_string(cosmos_db_connection_string)
+    database_name = ""
+    container_name = ""
+
+    database = client.get_database_client(database_name)
+    container = database.get_container_client(container_name)
+
+    log_entry = {
+        'name': name,
+        'style': style,
+        'address': address,
+        'is_veg': is_veg,
+        'is_open': is_open,
+        'timestamp': str(datetime.datetime.utcnow())
+    }
+
+    container.upsert_item(log_entry)
+
+    recommendation = {
+        'restaurantRecommendation': {
+            'name': 'Pizza hut',
+            'style': 'Italian',
+            'address': 'wherever street 99, somewhere',
+            'openHour': 9,
+            'closeHour': 23,
+            'vegetarian': 'yes'
+        }
+    }
+
+    current_hour = datetime.datetime.utcnow().hour
+    recommendation['restaurantRecommendation']['isOpen'] = recommendation['restaurantRecommendation']['openHour'] <= current_hour < recommendation['restaurantRecommendation']['closeHour']
+
+    return func.HttpResponse(json.dumps(recommendation), mimetype="application/json")
