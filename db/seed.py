24897544c51c4099aa79d9bd803ba81c21a8aaab
noam.miron <noam.e.miron@gmail.com>
2023-12-07 15:20:09 +0200

    debugging connection string

--- db/seed.py
+++ db/seed.py
@@ -17,7 +17,7 @@ def seed_data(client, data):
 if __name__ == "__main__":
     with open("seed_data.json", "r") as file:
         seeding_data = json.load(file)
-
+    print(cosmosdb_connection_string)
     client = CosmosClient.from_connection_string(cosmosdb_connection_string)
 
     seed_data(client, seeding_data)
