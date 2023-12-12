aa2496be31291ef4c55b92d2ae6bd5683e4de72a
noam.miron <noam.e.miron@gmail.com>
2023-12-12 14:02:39 +0200

    removed id from db query

--- function/src/function_app.py
+++ function/src/function_app.py
@@ -9,7 +9,6 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.cosmos_db_input(arg_name="documents", 
                      database_name=environ.get("COSMOSDB_DATABASE_NAME"),
                      collection_name=environ.get("COSMOSDB_CONTAINER_NAME"),
-                     id="id",
                      partition_key="/id",
                      connection_string_setting="MyAccount_COSMOSDB")
 @app.cosmos_db_output(arg_name="outputDocument",
