07025daac65eefb4e0bae71bc8d38e9938c16f29
noam.miron <noam.e.miron@gmail.com>
2024-01-15 12:13:25 +0200

    removed function db dependecy for testing

--- function/src/function_app.py
+++ function/src/function_app.py
@@ -6,15 +6,15 @@ from os import environ
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="api_function")
-@app.cosmos_db_input(arg_name="documents", 
-                     database_name=environ.get("COSMOSDB_DATABASE_NAME"),
-                     collection_name=environ.get("COSMOSDB_CONTAINER_NAME"),
-                     partition_key="/id",
-                     connection_string_setting="MyAccount_COSMOSDB")
-@app.cosmos_db_output(arg_name="outputDocument",
-                      database_name=environ.get("COSMOSDB_DATABASE_NAME"),
-                      collection_name=environ.get("COSMOSDB_CONTAINER_NAME"),
-                      connection_string_setting=environ.get("COSMOSDB_CONNECTION_STRING"))
+# @app.cosmos_db_input(arg_name="documents", 
+#                      database_name=environ.get("COSMOSDB_DATABASE_NAME"),
+#                      collection_name=environ.get("COSMOSDB_CONTAINER_NAME"),
+#                      partition_key="/id",
+#                      connection_string_setting="MyAccount_COSMOSDB")
+# @app.cosmos_db_output(arg_name="outputDocument",
+#                       database_name=environ.get("COSMOSDB_DATABASE_NAME"),
+#                       collection_name=environ.get("COSMOSDB_CONTAINER_NAME"),
+#                       connection_string_setting=environ.get("COSMOSDB_CONNECTION_STRING"))
 def api_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
   
